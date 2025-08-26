deaca8e06159e24b32a0b9d0ec6752c3a3bef466
Ajit Pratap Singh <ajitpratap.singh@pixis.ai>
2025-08-26 16:33:11 +0530

    fix: remove duplicate imports and add nolint directives
    - Remove duplicate imports of google_ads and meta_ads packages
    - Add nolint:stylecheck directives to mmap_darwin.go constants
    - Fix import organization in sources.go
    
    All linting issues have been checked locally before pushing.

--- pkg/connector/sources/sources.go
+++ pkg/connector/sources/sources.go
@@ -7,14 +7,13 @@ import (
 
 	// Import all source connectors to trigger init() registration
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/csv"
-	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/google_ads"
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/json"
-	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/meta_ads"
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/mongodb_cdc"
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/mysql_cdc"
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/postgresql"
 	_ "github.com/ajitpratap0/nebula/pkg/connector/sources/postgresql_cdc"
 
+	// Import with aliases for functions
 	googleads "github.com/ajitpratap0/nebula/pkg/connector/sources/google_ads"
 	metaads "github.com/ajitpratap0/nebula/pkg/connector/sources/meta_ads"
 )
